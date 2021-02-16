ce2649d1b205ec378fc5108f0cf5dc5a842eb4dd
furqoncreative <furqoncreative24@gmail.com>
2021-02-16 16:33:15 +0700

    Initial commit

---
+++ Kotlin Fundamental/Data Type/Character/src/App.kt
@@ -0,0 +1,15 @@
+// main function
+fun main() {
+    var vocal = 'A'
+
+    println("Vocal ${vocal++}")
+    println("Vocal ${vocal++}")
+    println("Vocal ${vocal++}")
+    println("Vocal ${vocal++}")
+    println("Vocal ${vocal--}")
+    println("Vocal ${vocal--}")
+    println("Vocal ${vocal--}")
+    println("Vocal ${vocal--}")
+    println("Vocal ${vocal--}")
+
+}
